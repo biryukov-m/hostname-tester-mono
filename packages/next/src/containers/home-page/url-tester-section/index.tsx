a7e8d04901b996de18fa785b26f5a5b6f84673a4
Max <biryukov.mm@gmail.com>
2024-01-25 17:25:22 +0200

    feat: Implemented base functionality on next js

---
+++ packages/next/src/containers/home-page/url-tester-section/index.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { useState } from 'react';
+import { Box } from '@mui/material';
+import { IResult } from '@/types/types';
+import { CircularProgressWithLabel } from '@/components/CircularProgressWithLabel';
+import { CustomizedSnackbars } from '@/components/CustomizedSnackbars';
+import { UrlInput } from './url-input';
+import { UrlResults } from './url-results';
+
+export const UrlTesterSection = () => {
+  const [hostsInput, setHostsInput] = useState('');
+  const [results, setResults] = useState<IResult[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [snackbar, setSnackbar] = useState(false);
+
+  return (
+    <>
+      {!isLoading && (
+        <UrlInput
+          {...{
+            hostsInput,
+            setHostsInput,
+            setResults,
+            isLoading,
+            setIsLoading,
+            progress,
+            setProgress,
+            setSnackbar
+          }}
+        />
+      )}
+      <Box>
+        {isLoading ? (
+          <Box
+            sx={{
+              display: 'flex',
+
+              alignItems: 'center',
+              gap: '18px'
+            }}
+          >
+            <div>
+              <CircularProgressWithLabel value={progress} />
+            </div>
+            <h4>Триває тестування...</h4>
+          </Box>
+        ) : (
+          <h2>Результати</h2>
+        )}
+        <UrlResults {...{ results, progress, isLoading }} />
+      </Box>
+      <CustomizedSnackbars {...{ open: snackbar, setOpen: setSnackbar }} />
+    </>
+  );
+};
