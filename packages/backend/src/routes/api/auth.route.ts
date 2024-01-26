a04a16d4765f4798af11897c7501f099a9ba3b00
Max <biryukov.mm@gmail.com>
2024-01-26 17:49:02 +0200

    feat: Add routes for auth add password and get all passwords

--- packages/backend/src/routes/api/auth.route.ts
+++ packages/backend/src/routes/api/auth.route.ts
@@ -1,4 +1,6 @@
 import { Request, Response, Router } from 'express';
+import { tryCatch } from '../../middleware/try-catch.middleware';
+import authController from '../../controllers/auth.controller';
 
 const router = Router();
 
@@ -9,11 +11,19 @@ router.post('/register', async (_req: Request, res: Response) => {
   res.send('Add registration logic here');
 });
 
-// @route   POST api/auth
+// @route   POST api/auth/add
 // @desc    Authorize user given their email and password, returns the token upon successful registration
 // @access  Public
-router.post('/auth', async (_req: Request, res: Response) => {
-  res.send('Add auth logic here');
-});
+router.post('/add', tryCatch(authController.addPassword.bind(authController)));
+
+// @route   POST api/auth/
+// @desc    Authorize user given their email and password, returns the token upon successful registration
+// @access  Public
+router.get('', tryCatch(authController.getAllPasswords.bind(authController)));
+
+// @route   POST api/auth/
+// @desc    Authorize user given their email and password, returns the token upon successful registration
+// @access  Public
+router.post('', tryCatch(authController.auth.bind(authController)));
 
 export default router;
