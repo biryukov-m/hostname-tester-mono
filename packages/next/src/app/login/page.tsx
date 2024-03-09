0f2432f62b2dff9a0f297046667df52de97e293a
Max <biryukov.mm@gmail.com>
2024-03-10 00:25:24 +0200

    feat: Styled login page

--- packages/next/src/app/login/page.tsx
+++ packages/next/src/app/login/page.tsx
@@ -1,15 +1,17 @@
 'use client';
 
 import { authenticate } from '@/app/lib/actions';
+import { Alert, Button, Container, TextField } from '@mui/material';
+import { Box } from '@mui/system';
 import { useFormState, useFormStatus } from 'react-dom';
 
 function LoginButton() {
   const { pending } = useFormStatus();
 
   return (
-    <button aria-disabled={pending} type="submit">
+    <Button aria-disabled={pending} type="submit" variant="outlined">
       Login
-    </button>
+    </Button>
   );
 }
 
@@ -17,10 +19,47 @@ export default function Page() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
   return (
-    <form action={dispatch}>
-      <input type="password" name="password" placeholder="Password" required />
-      <div>{errorMessage && <p>{errorMessage}</p>}</div>
-      <LoginButton />
-    </form>
+    <Container>
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <form action={dispatch}>
+          <TextField
+            id="password"
+            label="password"
+            type="password"
+            name="password"
+            placeholder="*****"
+            required
+          />
+
+          {errorMessage && (
+            <Alert sx={{ my: 1 }} severity="error">
+              Ауторизація неуспішна
+            </Alert>
+          )}
+
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              m: 1
+            }}
+          >
+            <LoginButton />
+          </Box>
+        </form>
+      </Box>
+    </Container>
   );
 }
