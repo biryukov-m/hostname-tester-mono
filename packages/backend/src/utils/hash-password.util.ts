15dbb022e21d32fb29e24b4c7a01f81d2a7f64cb
Max <biryukov.mm@gmail.com>
2024-03-26 13:36:15 +0200

    ref: Removed backend repo and lerna for laboring next js package standalone instead

--- packages/backend/src/utils/hash-password.util.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt';
-
-async function hashPassword(string: string) {
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(string, salt);
-  return hash;
-}
-
-export default hashPassword;
